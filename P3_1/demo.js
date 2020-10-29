49494005b9f581803d0a062bfa2e77da00114e98
Froilan Enriquez <fenriquez31@cepnet.net>
2020-10-29 12:16:18 +0100

    Editing P3 files

--- P3_1/demo.js
+++ P3_1/demo.js
@@ -1,7 +1,32 @@
+let answer;
 
-function getName(){
-    let myString;
-    myString = document.getElementById("target").value;
-    document.getElementById("output").innerHTML = myString;
+function getNumber(getMessage){
+    window.answer = Math.floor((Math.random() * 100) + 1);
+    getMessage();
+	console.log(window.answer);
+    return window.answer;
+  
+ }
+
+ function getMessage(){
+    document.getElementById("display").innerHTML = "New number generated!";
+ }
+ 
+function compareNum(answer){
+    let number;
     
+    number = document.getElementById("target").value;
+    
+   if (number == answer){
+    document.getElementById("output").innerHTML = "You are correct! My number is " + answer;
+   } else if (number < answer){
+  	document.getElementById("output").innerHTML =  number + " is too low, try again! ";
+   } else{
+  	document.getElementById("output").innerHTML =  number + " is too high, try again! ";
+   }
+ 
 }
+
+getNumber(function (){
+    return 0;
+});
