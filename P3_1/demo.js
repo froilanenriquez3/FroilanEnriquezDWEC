2bdcf3e232e2cd1234ec042e022ed2e8c958d130
Froilan Enriquez <fenriquez31@cepnet.net>
2020-11-02 14:18:20 +0100

    Updated P3 exercise

--- P3_1/demo.js
+++ P3_1/demo.js
@@ -1,49 +1,68 @@
 //Function declarations
 
-function getNumber(message){
-    window.answer = Math.floor((Math.random() * 101) + 1);
+function resetGame(message){
+    window.answer = Math.floor((Math.random() * 100) + 1);
     window.tries = 0;
+    document.getElementById("submit").style.display = "inline-block";
     message();
 	console.log(window.answer);
  }
 
  function getMessage(){
-    document.getElementById("output").innerHTML = "New number generated! (Tries: 0)";
+    document.getElementById("output").innerHTML = "New number generated! (Tries left: 5)";
  }
  
+ function checkNum(callback1, callback2){
+    let number;
+    
+    number = document.getElementById("target").value;
+    if(number < 1 || number > 100){
+        document.getElementById("output").innerHTML = "Your guess is not valid! (Number is between 1 and 100)";
+    }else if(window.tries > 5) {
+        document.getElementById("output").innerHTML = "You're out of tries! Generate a new number to try again.";
+    } else{
+        callback1();
+        callback2();
+    }
+}
+
 function compareNum(){
     let number;
     number = document.getElementById("target").value;
     window.tries++;
     
    if (number == window.answer){
-    document.getElementById("output").innerHTML = "You are correct! My number is " + window.answer;
-   } else if (number < window.answer){
-  	document.getElementById("output").innerHTML =  number + " is too low, try again! ";
-   } else{
-  	document.getElementById("output").innerHTML =  number + " is too high, try again! ";
+    document.getElementById("output").innerHTML = "You are correct, my number is " + window.answer + "! You had " + (5 - window.tries) + " tries left to spare.";
+    document.getElementById("submit").style.display = "none";
+   } else if (number < window.answer && window.tries < 5){
+  	document.getElementById("output").innerHTML =  number + " is too low, try again! (Tries left: " + (5 - window.tries) + ")";
+   } else if(number > window.answer && window.tries < 5){
+  	document.getElementById("output").innerHTML =  number + " is too high, try again! (Tries left: " + (5 - window.tries) + ")";
+   } else if(window.tries == 5){
+    document.getElementById("output").innerHTML = "You're all out of tries! My number was " + window.answer+ ". Generate a new number to try again.";
    }
- 
-   document.getElementById("output").innerHTML += " (Tries : " + window.tries + ")";
+   
 }
 
-function checkNum(compare){
+
+function listPrevious(){
     let number;
     number = document.getElementById("target").value;
-    if(number < 1 || number > 100){
-        document.getElementById("output").innerHTML = "Your guess is not valid! (Number is between 1 and 100)";
-    } else{
-        compare();
-    }
+    document.getElementById("previous").innerHTML += number + " : ";
 }
 
-//main
+function resetList(){
+    document.getElementById("previous").innerHTML = ": ";
+}
+
+
+    //main
 
 let answer;
 let tries;
 
-getNumber(function (){
-        document.getElementById("output").innerHTML = "Initial number generated. (Tries: 0)"
+resetGame(function (){
+        document.getElementById("output").innerHTML = "Initial number generated. (Tries left: 5)"
 
     });
 
