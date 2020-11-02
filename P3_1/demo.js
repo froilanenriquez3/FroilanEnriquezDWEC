0059b0832e707a3f448e9da1d7bfec245d2cc298
Froilan Enriquez <fenriquez31@cepnet.net>
2020-11-02 13:24:56 +0100

    Updated P3 exercise

--- P3_1/demo.js
+++ P3_1/demo.js
@@ -1,32 +1,49 @@
-let answer;
+//Function declarations
 
-function getNumber(getMessage){
-    window.answer = Math.floor((Math.random() * 100) + 1);
-    getMessage();
+function getNumber(message){
+    window.answer = Math.floor((Math.random() * 101) + 1);
+    window.tries = 0;
+    message();
 	console.log(window.answer);
-    return window.answer;
-  
  }
 
  function getMessage(){
-    document.getElementById("output").innerHTML = "New number generated!";
+    document.getElementById("output").innerHTML = "New number generated! (Tries: 0)";
  }
  
-function compareNum(answer){
+function compareNum(){
     let number;
-    
     number = document.getElementById("target").value;
+    window.tries++;
     
-   if (number == answer){
-    document.getElementById("output").innerHTML = "You are correct! My number is " + answer;
-   } else if (number < answer){
+   if (number == window.answer){
+    document.getElementById("output").innerHTML = "You are correct! My number is " + window.answer;
+   } else if (number < window.answer){
   	document.getElementById("output").innerHTML =  number + " is too low, try again! ";
    } else{
   	document.getElementById("output").innerHTML =  number + " is too high, try again! ";
    }
  
+   document.getElementById("output").innerHTML += " (Tries : " + window.tries + ")";
 }
 
+function checkNum(compare){
+    let number;
+    number = document.getElementById("target").value;
+    if(number < 1 || number > 100){
+        document.getElementById("output").innerHTML = "Your guess is not valid! (Number is between 1 and 100)";
+    } else{
+        compare();
+    }
+}
+
+//main
+
+let answer;
+let tries;
+
 getNumber(function (){
-    return 0;
-});
+        document.getElementById("output").innerHTML = "Initial number generated. (Tries: 0)"
+
+    });
+
