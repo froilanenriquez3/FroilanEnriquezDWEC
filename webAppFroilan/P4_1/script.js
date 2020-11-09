9fcf7689600fb3fcce497c5a55066f34ad438bf2
Froilan Enriquez <fenriquez31@cepnet.net>
2020-11-09 21:35:03 +0100

    "Modifying P4 exercise"

--- webAppFroilan/P4_1/script.js
+++ webAppFroilan/P4_1/script.js
@@ -8,12 +8,12 @@ function addGrades() {
   if(grades[0] == -1) {
   	grades[0] = grade;
   } else{
-  	grades.push(grade);
+  	grades.push(grade); //1
   }
 }
 
 function removeGrades(){
-	grades.pop();
+	grades.pop(); //2
 }
 
 function nextStudent() {
@@ -47,23 +47,40 @@ let index = 0;
 }
 
 function sortGrades(){
-	grades.sort(function(a, b){return b-a});
+	grades.sort(function(a, b){return b-a}); //4
   document.getElementById("list").innerHTML = "";
   
-  grades.forEach(function (item){document.getElementById("list").innerHTML += item + "<br>";})
+  grades.forEach(function (item){document.getElementById("list").innerHTML += item + "<br>";}) //3
 }
 
 function displayGrades(){
 	document.getElementById("output").innerHTML = "";
-	grades.forEach(function (item){document.getElementById("output").innerHTML += item + "<br>";})
+	grades.forEach(function (item){document.getElementById("output").innerHTML += item + "<br>";}) //3
 }
 
 function averageGrades(){
 let average;
 let sum = 0;
-grades.forEach(function (item){sum += item});
+grades.forEach(function (item){
+sum = sum + Number(item);
+}); //3
+console.log(sum);
 average = sum/grades.length;
 	
 	document.getElementById("average").innerHTML = "The average grade of the students is: " + average;
+}
+
+function addingFunction(){
+let grade = document.getElementById("target").value;
+
+	if(grade < 0 || grade > 10){
+  	document.getElementById("feedback").innerHTML = "The grade must be between 0 and 10";
+  } else{
+  document.getElementById("feedback").innerHTML = "";
+addGrades();
+nextStudent();
+displayGrades();
+averageGrades();
+}
 
 }
