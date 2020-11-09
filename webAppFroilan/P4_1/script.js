9031e206589e48aec29a28d55986b074916b5b6c
Froilan Enriquez <fenriquez31@cepnet.net>
2020-11-09 21:17:10 +0100

    Created P4

---
+++ webAppFroilan/P4_1/script.js
@@ -0,0 +1,69 @@
+let grades = [-1];
+
+let number = 0;
+
+function addGrades() {
+	let grade = document.getElementById("target").value;
+  
+  if(grades[0] == -1) {
+  	grades[0] = grade;
+  } else{
+  	grades.push(grade);
+  }
+}
+
+function removeGrades(){
+	grades.pop();
+}
+
+function nextStudent() {
+	number ++;
+	document.getElementById("plea").innerHTML = "Enter the grade of student " + (number + 1);
+}
+
+function lastStudent() {
+	if (number > 0){
+	number --;
+  
+	document.getElementById("plea").innerHTML = "Enter the grade of student " + (number + 1);
+  }
+  
+}
+
+function highGrade(){
+let max = 0;
+let counter = 0;
+let index = 0;
+
+ do {
+ 	if(grades[counter] >= max){
+  	max = grades[counter];
+    index = counter;
+  }
+ 	counter ++;
+ } while(counter < grades.length);
+ 
+ document.getElementById("high").innerHTML = "The smartest student is student number " + (index + 1);
+}
+
+function sortGrades(){
+	grades.sort(function(a, b){return b-a});
+  document.getElementById("list").innerHTML = "";
+  
+  grades.forEach(function (item){document.getElementById("list").innerHTML += item + "<br>";})
+}
+
+function displayGrades(){
+	document.getElementById("output").innerHTML = "";
+	grades.forEach(function (item){document.getElementById("output").innerHTML += item + "<br>";})
+}
+
+function averageGrades(){
+let average;
+let sum = 0;
+grades.forEach(function (item){sum += item});
+average = sum/grades.length;
+	
+	document.getElementById("average").innerHTML = "The average grade of the students is: " + average;
+
+}
