2603038e084756ab59aba6e34f005c9ca50cc286
Froilan-examen <fenriquez31@cepnet.net>
2021-02-25 13:39:46 +0100

    Add custom controls for video, tweak scss

--- webAppFroilan/DIW_3_2/script.js
+++ webAppFroilan/DIW_3_2/script.js
@@ -1,21 +1,51 @@
 function toggleControls(){
     let videoElem = document.querySelector("#myVideo");
+    
     if (videoElem.controls === true){
-        videoElem.setAttribute("controls", "false");
-        document.querySelector("#controlsToggle").innerHTML = "Add controls";
-       
         videoElem.controls = false; 
+        document.querySelector("#controlsToggle").innerHTML = "Use default controls";
+        document.querySelector("#customControl").style.display = "block";
 
         //console.log("controls removed");
         //console.log(videoElem.controls);
     } else{
         videoElem.setAttribute("controls", "true");
-        document.querySelector("#controlsToggle").innerHTML = "Remove controls";
+        document.querySelector("#controlsToggle").innerHTML = "Use custom controls";
+        document.querySelector("#customControl").style.display = "none";
         
         //console.log("controls added");
         //console.log(videoElem.controls);
     }
     
-    
+}
+
+function skipTen(){
+    let videoElem = document.querySelector("#myVideo");
+    videoElem.currentTime += 10;
+
+}
+
+function rewindTen(){
+    let videoElem = document.querySelector("#myVideo");
+    videoElem.currentTime -= 10;
+
+}
+
+function playVideo(){
+    let videoElem = document.querySelector("#myVideo");
+    videoElem.play();
+
+    document.querySelector("#playVideo").innerHTML = "Pause video";
+    document.querySelector("#playVideo").removeEventListener("click", playVideo);
+    document.querySelector("#playVideo").addEventListener("click", pauseVideo);
+}
+
+function pauseVideo(){
+    let videoElem = document.querySelector("#myVideo");
+    videoElem.pause();
+
+    document.querySelector("#playVideo").innerHTML = "Pause video";
+    document.querySelector("#playVideo").removeEventListener("click", pauseVideo);
+    document.querySelector("#playVideo").addEventListener("click", playVideo);
 
 }
