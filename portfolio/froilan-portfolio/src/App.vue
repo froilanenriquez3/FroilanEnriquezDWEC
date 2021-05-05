a92e5d2a2c01f20fb7de4cd524431167fd6778be
froilanenriquez3 <fenriquezthree@gmail.com>
2021-05-05 14:41:02 +0200

    sample card component

--- portfolio/froilan-portfolio/src/App.vue
+++ portfolio/froilan-portfolio/src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <Navbar />
+    <Navbar id="navbar"/>
+    <Card id="card"/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/Navbar.vue";
+import Card from "./components/Card.vue";
 
 export default {
   name: "App",
   components: {
     // HelloWorld,
     Navbar,
+    Card
   },
 };
 </script>
@@ -27,5 +30,8 @@ export default {
   text-align: center;
   /* color: #2c3e50; */
   margin-top: 60px;
+
+ 
+
 }
 </style>
