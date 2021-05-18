83dc8de9b5e53934762000f3bc2eeeeeac1c5331
froilanenriquez3 <fenriquezthree@gmail.com>
2021-05-18 13:59:12 +0200

    soft skills card translation

--- portfolio/froilan-portfolio/src/components/SoftSkills.vue
+++ portfolio/froilan-portfolio/src/components/SoftSkills.vue
@@ -5,21 +5,21 @@
     </div>
     <div class="card-body bg-white text-black">
       <ul class="list-group">
-        <li class="list-group-item active" aria-current="true">
+        <li class="list-group-item active translate" id="teamworkHeader" aria-current="true">
           Teamwork
         </li>
-        <li class="list-group-item" align="justify">
+        <li class="list-group-item translate" id="teamworkText" align="justify">
           Through our center's project based learning model, I had the opportunity
           to work in two group projects. During these projects, I practiced my teamwork 
           skills and learned how to collaborate with others.
         </li>
-        <li class="list-group-item active">Responsibility</li>
-        <li class="list-group-item" align='justify'>
+        <li class="list-group-item active translate" id="responsibilityHeader">Responsibility</li>
+        <li class="list-group-item translate" id="responsibilityText" align='justify'>
           I strive to be accountable, dependable, and reliable. I know that when 
           you are entrusted with something, it is important to keep that trust and not lose it. 
         </li>
-        <li class="list-group-item active">Organization</li>
-        <li class="list-group-item" align="justify">
+        <li class="list-group-item active translate" id="organizationHeader">Organization</li>
+        <li class="list-group-item translate" id="organizationText" align="justify">
           An important trait when it comes to working is the ability to stay organized.
           This makes the workflow easier, and so I strive to stay organized in every aspect
           of my work. Whether it means maintaining my physical workspace, or keeping my
